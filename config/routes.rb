ecbeade7ee62d5b35c94705b14006ff6f0a026a8
tabatalisboa <tabatalisboa@pm.me>
2019-06-28 15:25:13 -0300

    Delete HTML components

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   devise_for :users
-  # root to: 'pages#home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
