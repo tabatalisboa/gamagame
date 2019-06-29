4a742b46b9128082092db3aa4ccce064412dd2f3
tabatalisboa <tabatalisboa@pm.me>
2019-06-28 22:22:47 -0300

    Make migration to Game

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190627232258) do
+ActiveRecord::Schema.define(version: 20190629001832) do
 
   create_table "games", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "completed", default: false
+    t.boolean "completed"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
