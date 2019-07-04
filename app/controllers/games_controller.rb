7614caaad92084bc14ee585ec744ac81344ae8e5
tabatalisboa <tabatalisboa@pm.me>
2019-07-04 00:33:44 -0300

    Add game routes and GamesController

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,21 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
+  resources :games
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
